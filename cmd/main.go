b275423059c529f1a4cd569803b3af3a726f938c
talantone <talgatsitd@gmail.com>
2022-06-22 15:49:26 +0200

    Added something new

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	tasks_app "tasks-list-project"
 	"tasks-list-project/pkg/handler"
+	"tasks-list-project/pkg/repository"
+	"tasks-list-project/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	server := new(tasks_app.Server)
-	if err := server.Run("8000", handler.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ pkg/service/service.go
@@ -0,0 +1,22 @@
+package service
+
+import "tasks-list-project/pkg/repository"
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(r *repository.Repository) *Service {
+	return &Service{}
+}
